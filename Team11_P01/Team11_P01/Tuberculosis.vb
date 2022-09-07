9e2febacbb6d25375c6f3ac059dd366fc1ea9acb
Mphojim <112944799+Mphojim@users.noreply.github.com>
2022-09-07 21:05:06 +0200

    Adding TB Class
    This is Tuberculosis Class.

---
+++ Team11_P01/Team11_P01/Tuberculosis.vb
@@ -0,0 +1,16 @@
+﻿' ***************************************************************** 
+' Team Number: 11 
+' Team Member 1 Details: Matlhoko, T (217026727) 
+' Team Member 2 Details: Surname, Initials (Student #) 
+' Team Member 3 Details: Surname, Initials (Student #) 
+' Team Member 4 Details: e.g. Smith, J (202000001) 
+' Practical: Team Project 
+' Class name: (Disease) 
+' *****************************************************************
+Option Strict On
+Option Explicit On
+Option Infer Off
+
+Public Class Tuberculosis
+
+End Class
