adc9fdc5f8a11b7ac9cfe72a0cb1c753aa28b24c
Mphojim <112944799+Mphojim@users.noreply.github.com>
2022-09-08 12:26:18 +0200

    Attributes of TB (222137338)
    Stage attribute stores the stage of TB patient. Then Vaccinated attribute store Vaccination status of the TB patient.

--- Team11_P01/Team11_P01/Tuberculosis.vb
+++ Team11_P01/Team11_P01/Tuberculosis.vb
@@ -12,5 +12,40 @@ Option Explicit On
 Option Infer Off
 
 Public Class Tuberculosis
+    'Attributes of TB
+    Inherits Disease
 
+    Private _Stage As Integer
+    Private _Vaccinated As Boolean 'To get weather the person is vaccinated or not.
+
+    ' Constructor 
+    Public Sub New(numIll As Integer, numCured As Integer,
+                   numDied As Integer)
+        MyBase.New(numIll, numCured, numDied)
+    End Sub
+
+    ' Property methods 
+
+    Public Property Stage() As Integer
+        Get
+            Return _Stage
+        End Get
+        Set(value As Integer)
+            If value >= 1 Then
+                _Stage = value
+            End If
+        End Set
+    End Property
+
+    Public Property Vaccinated() As Boolean
+        Get
+            Return _Vaccinated
+        End Get
+        Set(value As Boolean)
+            _Vaccinated = value
+        End Set
+    End Property
+    Public Overrides Function DetermineTreatment() As String
+        Throw New NotImplementedException()
+    End Function
 End Class
