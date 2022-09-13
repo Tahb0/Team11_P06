7cebec23adbabe0ac7df31a69ffcef0f0066c4cc
Mphojim <112944799+Mphojim@users.noreply.github.com>
2022-09-13 21:05:10 +0200

    Quick update

--- Team11_P01/Team11_P01/Form1.Designer.vb
+++ Team11_P01/Team11_P01/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmDiseaseMonitor
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,32 +20,18 @@ Partial Class frmDiseaseMonitor
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.GrdDiseaseMonitor = New UJGrid.UJGrid()
         Me.SuspendLayout()
-        '
-        'GrdDiseaseMonitor
-        '
-        Me.GrdDiseaseMonitor.FixedCols = 1
-        Me.GrdDiseaseMonitor.FixedRows = 1
-        Me.GrdDiseaseMonitor.Location = New System.Drawing.Point(237, 117)
-        Me.GrdDiseaseMonitor.Name = "GrdDiseaseMonitor"
-        Me.GrdDiseaseMonitor.Scrollbars = System.Windows.Forms.ScrollBars.Both
-        Me.GrdDiseaseMonitor.Size = New System.Drawing.Size(229, 150)
-        Me.GrdDiseaseMonitor.TabIndex = 0
         '
         'frmDiseaseMonitor
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.GrdDiseaseMonitor)
         Me.Name = "frmDiseaseMonitor"
         Me.Text = "Disease Monitor "
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents GrdDiseaseMonitor As UJGrid.UJGrid
 End Class
