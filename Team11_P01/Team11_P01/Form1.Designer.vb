a69bbd8ac53756bc6beef8e0548f3c7571688108
Tahb0 <97204337+Tahb0@users.noreply.github.com>
2022-09-15 00:08:50 +0200

    Form Update
    updated the form and the designer
    
    Co-Authored-By: LUYANDA2004 <112948676+LUYANDA2004@users.noreply.github.com>
    Co-Authored-By: Mphojim <112944799+Mphojim@users.noreply.github.com>
    Co-Authored-By: Mtho-kozisi <112950046+Mtho-kozisi@users.noreply.github.com>

--- Team11_P01/Team11_P01/Form1.Designer.vb
+++ Team11_P01/Team11_P01/Form1.Designer.vb
@@ -22,42 +22,54 @@ Partial Class frmDiseaseMonitor
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.UjGrid1 = New UJGrid.UJGrid()
+        Me.btnCaputure = New System.Windows.Forms.Button()
+        Me.grdPatientMonitor = New UJGrid.UJGrid()
+        Me.btnSetUp = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnCaputure
         '
-        Me.Button1.Location = New System.Drawing.Point(25, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(119, 41)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnCaputure.Location = New System.Drawing.Point(24, 64)
+        Me.btnCaputure.Name = "btnCaputure"
+        Me.btnCaputure.Size = New System.Drawing.Size(119, 41)
+        Me.btnCaputure.TabIndex = 0
+        Me.btnCaputure.Text = "Capture Input"
+        Me.btnCaputure.UseVisualStyleBackColor = True
         '
-        'UjGrid1
+        'grdPatientMonitor
         '
-        Me.UjGrid1.FixedCols = 1
-        Me.UjGrid1.FixedRows = 1
-        Me.UjGrid1.Location = New System.Drawing.Point(174, 12)
-        Me.UjGrid1.Name = "UjGrid1"
-        Me.UjGrid1.Scrollbars = System.Windows.Forms.ScrollBars.Both
-        Me.UjGrid1.Size = New System.Drawing.Size(229, 150)
-        Me.UjGrid1.TabIndex = 1
+        Me.grdPatientMonitor.FixedCols = 1
+        Me.grdPatientMonitor.FixedRows = 1
+        Me.grdPatientMonitor.Location = New System.Drawing.Point(174, 12)
+        Me.grdPatientMonitor.Name = "grdPatientMonitor"
+        Me.grdPatientMonitor.Scrollbars = System.Windows.Forms.ScrollBars.Both
+        Me.grdPatientMonitor.Size = New System.Drawing.Size(229, 150)
+        Me.grdPatientMonitor.TabIndex = 1
+        '
+        'btnSetUp
+        '
+        Me.btnSetUp.Location = New System.Drawing.Point(24, 17)
+        Me.btnSetUp.Name = "btnSetUp"
+        Me.btnSetUp.Size = New System.Drawing.Size(119, 41)
+        Me.btnSetUp.TabIndex = 2
+        Me.btnSetUp.Text = "Set Up"
+        Me.btnSetUp.UseVisualStyleBackColor = True
         '
         'frmDiseaseMonitor
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.UjGrid1)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnSetUp)
+        Me.Controls.Add(Me.grdPatientMonitor)
+        Me.Controls.Add(Me.btnCaputure)
         Me.Name = "frmDiseaseMonitor"
         Me.Text = "Patients Records "
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents UjGrid1 As UJGrid.UJGrid
+    Friend WithEvents btnCaputure As Button
+    Friend WithEvents grdPatientMonitor As UJGrid.UJGrid
+    Friend WithEvents btnSetUp As Button
 End Class
--- Team11_P01/Team11_P01/Form1.vb
+++ Team11_P01/Team11_P01/Form1.vb
@@ -10,6 +10,86 @@
 Option Strict On
 Option Explicit On
 Option Infer Off
+
+Enum DiseaseRepresentation
+    Tb = 1
+    Cancer
+    Aids
+End Enum
+
 Public Class frmDiseaseMonitor
+
+    Private nP As Integer
+    Private objPatient() As Patient
+
+    'a sub to place text in grid
+    Private Sub PIG(r As Integer, c As Integer, t As String)
+
+        grdPatientMonitor.Row = r
+        grdPatientMonitor.Col = c
+        grdPatientMonitor.Text = t
+
+    End Sub
+
+    Private Sub btnSetUp_Click(sender As Object, e As EventArgs) Handles btnSetUp.Click
+        'prompt the user for the number of patients:
+        nP = CInt(InputBox("The number of Patients: "))
+
+        ReDim objPatient(nP)
+
+        For p As Integer = 1 To nP
+
+            'prompt the user for the number of diseases the patient has
+            Dim nD As Integer = CInt(InputBox("The number of Diseases for patient " &
+                                              p & ":"))
+
+            objPatient(p) = New Patient(nD) 'redims the disease array/abs-class
+
+        Next
+    End Sub
+
+    Private Sub btnCaputure_Click(sender As Object, e As EventArgs) Handles btnCaputure.Click
+
+        'prompt the user for infomation about the patient
+        For p As Integer = 1 To nP
+            objPatient(p).PName = InputBox("Patient " & p & "name: ")
+            objPatient(p).PWeight = CDbl("Patient " & p & "weight(kg): ")
+
+            For d As Integer = 1 To objPatient(p).numDiseases
+
+                'prompt the user for the number disease 
+                Dim diseaseInput As Integer = CInt(InputBox("Disease " & d & " is: " & vbNewLine &
+                                                            "TB = 1" & vbNewLine &
+                                                            "Cancer = 2" & vbNewLine &
+                                                            "HIV/Aids = 3"))
+
+                If diseaseInput = DiseaseRepresentation.Tb Then
+                    'create a tb obj for the patient
+                    Dim objTb As New Tuberculosis
+                    objTb.Stage = CInt(InputBox("What stage of tb(1-4):"))
+
+                    'upcasting
+                    objPatient(p).PDisease(d) = objTb
+
+                Else
+                    If diseaseInput = DiseaseRepresentation.Cancer Then
+                        'create a cancer obj for the patient
+                        Dim objCancer As New Cancer
+                        objCancer.Stage = CInt(InputBox("What stage of Cancer(1-4):"))
+
+                        'upcasting
+                        objPatient(p).PDisease(d) = objCancer
+                    Else
+                        If diseaseInput = DiseaseRepresentation.Aids Then
+                            'create a cancer obj for the patient
+
+                        End If
+                    End If
+                End If
+            Next d
+
+        Next p
+    End Sub
+
 
 End Class
